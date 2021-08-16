9cb95d84dc8bfc0b965df1898d549f4ce994529b
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-08-16 11:43:43 +0700

    add responsive mobile menu behavior

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+//open and close modal behavior
 const buyBtns = document.querySelectorAll('.js-buy-ticket');
 const modal = document.querySelector('.js-modal');
 const modalContainer = document.querySelector('.js-modal-container');
@@ -19,3 +20,26 @@ modal.addEventListener('click', hideBuyTicket);
 modalContainer.addEventListener('click', (event) => {
     event.stopPropagation();
 });
+
+//open and close mobile menu behavior
+var header = document.getElementById('header');
+var mobileBtn = document.querySelector('#header .mobile-menu-icon');
+var headerHeight = header.clientHeight;
+
+mobileBtn.addEventListener('click', ()=>{
+    var isClosed = headerHeight === header.clientHeight;
+    if(isClosed){
+        header.style.height = 'auto';
+    }else{
+        header.style.height = null;
+    }
+})
+
+//close menu and move to section of page
+var menuItems = document.querySelectorAll('ul#nav li a[href*="#"]');
+
+for(var menuItem of menuItems){
+    menuItem.onclick = function(){
+        header.style.height = null;
+    }
+}
